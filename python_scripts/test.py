d351cbfec1a45c2ea15cb5613cfc7408f60792c2
Dustin McBride <dustinbm@gmail.com>
2019-03-02 20:49:12 -0800

    script testing

--- python_scripts/test.py
+++ python_scripts/test.py
@@ -1,1 +1,9 @@
 logger.info('Python Test Script Running')
+
+entity_ids  = data.get('entity_ids')
+
+logger.info("entity_ids %s", entity_ids)
+
+sun = hass.states.get('sun.sun')
+
+logger.info("sun %s", sun.attributes.get("elevation"))
